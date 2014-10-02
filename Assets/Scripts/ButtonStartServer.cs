5e2c3f67f04365e433ccca936fb543cfa699e16e
vbas <valentinbas.dev@gmail.com>
2014-10-02 17:30:09 +0200

    first try server

---
+++ Assets/Scripts/ButtonStartServer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class ButtonStartServer : MonoBehaviour
+{
+    private AsynchronousSocketListener  _server;
+    private Thread                      _thread;
+
+    void Clicked()
+    {
+        Debug.Log("starting server...");
+        _server = new AsynchronousSocketListener();
+        _thread = new Thread(_server.StartListening);
+        _thread.Start();
+    }
+
+    void OnApplicationQuit()
+    {
+        if (_server != null)
+        {
+            _server.StopListening();
+            _thread.Join();
+        }
+    }
+}
